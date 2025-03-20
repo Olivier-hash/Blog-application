e9122e01e7a9734e723ba55a12310b861e33a70d
olivier Rukundo <rr.olivier01@gmail.com>
2025-03-20 23:11:16 +0200

    error fixed successfully

---
+++ my-react-app/src/components/pages/settings/Settings.jsx
@@ -0,0 +1,14 @@
+import Sidebar from '../../sidebar/Sidebar'
+import './settings.css'
+
+function Settings() {
+  return (
+    <div className='settings'>
+      <div className="settiwrapper">
+        <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Settings
