55292286c7c86c6cfa87772a4465434b3c32d22d
olivier Rukundo <rr.olivier01@gmail.com>
2025-03-21 12:04:12 +0200

    updated second time

--- my-react-app/src/components/pages/settings/Settings.jsx
+++ my-react-app/src/components/pages/settings/Settings.jsx
@@ -1,12 +1,34 @@
 import Sidebar from '../../sidebar/Sidebar'
 import './settings.css'
+import img from '../../../assets/team5.jpeg';
 
 function Settings() {
   return (
     <div className='settings'>
-      <div className="settiwrapper">
-        <Sidebar/>
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+             <span className="settingsUpdateTitle">Update your Account</span>
+             <span className="settingsDeleteTitle">Delete your Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile picture</label>
+          <div className="settingsPP">
+                <img src={img} alt="" />
+             <label htmlFor="fileInput">
+                <i className="settingsPPIcon fa-regular fa-circle-user"></i>
+             </label>
+             <input type="file" id='fileInput' style={{display:"none"}} />
+          </div>
+          <label >Username</label>
+          <input type="text" placeholder='olivier-hash' />
+          <label >Email</label>
+          <input type="email" placeholder='olivierhash@gmail.com' />
+          <label >Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
       </div>
+      <Sidebar/>
     </div>
   )
 }
