c1cad9441d569bbed83c446490c0ba953ecb2dbc
olivier Rukundo <rr.olivier01@gmail.com>
2025-03-21 20:04:07 +0200

    fixed Router

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,12 +6,15 @@ import Write from './components/pages/write/Write'
 import Settings from './components/pages/settings/Settings'
 import Login from './components/pages/login/Login'
 import Register from './components/pages/register/Register'
+import { Routes, Route, createBrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <TopBar/>
-      <Register/>
+    <Routes>
+      <Route path='' element={<TopBar/>}/>
+      <Route path='' element={<Register/>}/> 
+    </Routes>  
     </>
   )
 }
