4997b5e7db8659097663a9297d5d25e3b0fe43cd
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-10 22:15:33 +0200

    responsiveness of footer section on different screen added

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='container w-full mx-auto bg-gray-800'>
-        <div>
-            <div className='flex justify-between'>
+    <div className='pt-10 px-4 md:px-20 lg:px-32 w-full bg-gray-800'>
+        
+            <div className='container flex justify-between md:flex-row mx-auto'>
                 {/* three columns */}
-                <div className='flex flex-col'>info
-                    <h2>we are ready to deliver</h2>
-                    <h2>we work by profession</h2>
-                    <h2>Every service is trusted</h2>
+                <div className='w-full flex flex-col'>
+                    <h3 className='text-gray-400 text-lg'>info</h3>
+                    <p className='text-gray-400'>we are ready to deliver</p>
+                    <p className='text-gray-400'>we work by profession</p>
+                    <p className='text-gray-400'>Every service is trusted</p>
                 </div>
-                <div>Links</div>
+                <div className='flex '>Links
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Contact us</a>
+                </div>
                 <div>Subscription</div>
             </div>
-        </div>
+    
     </div>
   )
 }
