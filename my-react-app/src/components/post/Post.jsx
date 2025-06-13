99c9c3fe540c2e79eb3e2a4919f7ee5e34655794
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-13 18:03:15 +0200

    Post section edited

--- my-react-app/src/components/post/Post.jsx
+++ my-react-app/src/components/post/Post.jsx
@@ -1,41 +1,66 @@
 import img from '../../assets/team(i).jpeg'
-import './post.css'
+
+
+
+import React from 'react'
+
 
 function Post() {
   return (
-    <div className='container mx-auto pt-10 py-4 md:px-20 lg:px-32 overflow-hidden'>
-        <img className='' src={img} alt="" />
-        <div className=" bottom-5  justify-center">
-            <div className="inline-block flex flex-col bg-white w-3/4 px-4 py-2 shadow-md">
-                <span className="text-xl font-semibold text-gray-800">Music</span>
-                <span className="text-xl font-semibold text-gray-800">Life</span>
-            </div>
-            <span className="">This is sampled text to be replaced</span>
-            <hr />
-            <span className="">1 hour ago</span>
+    <div className='container w-full mx-auto pt-10 py-4 md:px-20 lg:px-32 overflow-hidden'>
+      <div className='md:w-1/3 bg-[#D9D9D9] rounded shadow-lg'>
+        
+        {/* Title */}
+        <div className='pt-4 px-4 font-bold text-2xl'>
+          <h2>Most Viewed Post</h2>
         </div>
-        <p className=''>
-          This is the place for the post description
-        </p>
-        {/* Sampled from Real estate */}
 
-        <div className='flex justify-end items-center mb-8'>
+        {/* Image */}
+        <img
+          className='w-full h-auto object-cover rounded-t'
+          src={img}
+          alt="Most viewed post"
+        />
 
-          <button onClick={prevProject}
-            className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
-            <img src={assets.left_arrow} alt="previous" />
-          </button>
-
-          <button onClick={nextProject}
-            className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
-            <img src={assets.right_arrow} alt="Next" />
-          </button>
-
+        {/* Buttons Section */}
+        <div className='flex gap-2 pt-2 px-4 bg-white pb-4'>
+          <button className='border px-4 py-2 rounded'>23,473</button>
+          <button className='border px-4 py-2 rounded'>10,775</button>
         </div>
-        {/* end */}
+        
+      </div>
     </div>
-    
   )
 }
-
 export default Post
+
+
+
+
+// import img from '../../assets/team(i).jpeg'
+// import './post.css'
+
+// function Post() {
+//   return (
+//     <div className='container mx-auto pt-10 py-4 md:px-20 lg:px-32 overflow-hidden'>
+//         <img className='' src={img} alt="" />
+//         <div className=" bottom-5  justify-center">
+//             <div className="inline-block flex flex-col bg-white w-3/4 px-4 py-2 shadow-md">
+//                 <span className="text-xl font-semibold text-gray-800">Music</span>
+//                 <span className="text-xl font-semibold text-gray-800">Life</span>
+//             </div>
+//             <span className="">
+              
+//             </span>
+//             <hr />
+//             <span className="">1 hour ago</span>
+//         </div>
+//         <p className=''>
+//           This is the place for the post description
+//         </p>
+//     </div>
+    
+//   )
+// }
+
+// export default Post
