3419b130ac7f49abd9b102d6840044ed9e46680a
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-14 22:20:43 +0200

    The SLider button for showing cards added

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -1,52 +1,104 @@
-import React from 'react'
-import { assets, testimonialsData } from '../assets/assets'
+import React, { useEffect, useState } from 'react'
+import {assets, projectsData} from '../assets/assets'
 import { motion } from "motion/react"
 
 
+function Projects() {
 
-function Testimonials() {
+  // scrolling functionality for slider buttons
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardsToShow] = useState(1);
+
+
+  useEffect(()=>{
+    const updateCardsToShow = ()=> {
+         if(window.innerWidth >= 1024) {   // for large screens
+            setCardsToShow(projectsData.length);
+         }else{
+            setCardsToShow(1)
+         };      
+    };
+     updateCardsToShow();
+
+     //Event listener to update cardsToshow on window resize
+
+     window.addEventListener('resize', updateCardsToShow);
+
+     // Cleanup function to remove the event Listener
+     return() =>{
+      window.removeEventListener('resize', updateCardsToShow)
+     }
+  },[])
+
+
+  // function to handle next and previous project navigation
+
+  const nextProject = ()=> {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length)
+  }
+    const prevProject = ()=> {
+    setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1)    // if ur at the first project(index 0) it jumps to the last project
+                                                                                                // rember array are zero indexed that's why we use projectData.length -1
+  }
+
   return (
-    <motion.div 
+    <motion.div
     
-    // Animation properties
-    initial={{opacity:0, x:100}}
+     // Animation properties
+    initial={{opacity:0, x:-200}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
     viewport={{once:true}}
+     
     
     
-    
-    
-    className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonies'>
-       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Customer 
-         <span className='underline underline-offset-4 decoration-1 under font-light'>Testimonials</span>
-       </h1>
-         <p className='text-center text-gray-500 mb-12 mx-w-80 mx-auto'>
-           Real Stories from Those who found Home with Us</p> 
-             
-             {/* Testimonial Data */}
-        <div className='flex flex-wrap justify-center items-center gap-4 '>
-            {testimonialsData.map((testimonial, index) => (
-                <div key={index} className='max-w-[300px] shadow-lg rounded px-8 py-12 text-center'>
-                    <img className='w-20 h-20 rounded-full mx-auto' src={testimonial.image} alt={testimonial.alt} />
+    className='container  mx-auto  py-4 pt-20 px-6 md:px-20 lg:px-32 w-full overflow-hidden' id='Projects'>
+        <h1 className='text-center justify-end mb-5 text-2xl sm:text-4xl font-bold mb-2'>Projects 
+            <span className='underline underline-offset-4 decorated-1 under'>Completed</span>
+        </h1>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting spaces, Building Legacies-Explore our portifolio</p>
 
-                    {/* Description */}
-                    <h2 className='text-xl text-gray-700 font-medium'>{testimonial.name}</h2>
-                    <p className='text-gray-500 mb-4 text-sm'>{testimonial.title}</p>
+        {/* slider buttons */}
 
-                    <div className='flex justify-center items-center gap-1 text-red-500 mb-4'>
-                        {Array.from({length: testimonial.rating}, (item, index)=> (
-                            <img key={index} src={assets.star_icon} alt="" />
-                        ))}
-                    </div>
-                    
-                    {/*  Testimonial text*/}
-                    <p className='text-gray-600'>{testimonial.text}</p>
+        <div className='flex justify-end items-center mb-8'>
+
+          <button onClick={prevProject}
+            className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
+            <img src={assets.left_arrow} alt="previous" />
+          </button>
+
+          <button onClick={nextProject}
+            className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
+            <img src={assets.right_arrow} alt="Next" />
+          </button>
+
+        </div>
+
+        {/* project slider container */}
+        <div className='overflow-hidden '>
+
+          <div className='flex gap-8 transition-transform duration-500 ease-in-out'
+          style={{transform: `translateX(-${(currentIndex * 100) / cardsToShow}%)`}}
+          >
+            {projectsData.map((project, index)=>(
+              <div  key={index} className='relative flex-shrink-0 w-full sm:w-1/4'>
+                <img src={project.image} alt={project.title} className='w-full h-auto mb-14'/>
+                <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
+                  <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
+                       <h2 className='text-xl font-semibold text-gray-800'>
+                            {project.title}
+                      </h2> 
+                      <p className='text-gray-500 text-sm'>
+                            {project.price}  <span className='px-1'>|</span> {project.location}
+                      </p>              
+                  </div>
                 </div>
+              </div>
             ))}
+          </div>
         </div>
     </motion.div>
   )
 }
 
-export default Testimonials
+export default Projects
