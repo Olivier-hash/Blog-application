b27ef2228cd40a4cc2ed3f39e54f8d602348181d
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-16 21:20:56 +0200

    Updated layout change to Onepost.jsx

--- my-react-app/src/components/singlePost/OnePost.jsx
+++ my-react-app/src/components/singlePost/OnePost.jsx
@@ -1,3 +1,4 @@
+
 import img from '../../assets/team4.jpeg'
 
 function OnePost() {
@@ -17,7 +18,7 @@ function OnePost() {
           {/* Title + Edit Icons */}
           <div className="relative">
             <h1 className="text-2xl md:text-3xl font-bold mb-4">This is the Single Post Title</h1>
-            <div className="absolute right-0 flex gap-3 text-gray-600">
+            <div className="absolute top-0 right-0 flex gap-3 text-gray-600">
               <i className="fa-solid fa-pen-to-square cursor-pointer hover:text-blue-600"></i>
               <i className="fa-solid fa-trash-can cursor-pointer hover:text-red-600"></i>
             </div>
@@ -52,7 +53,70 @@ export default OnePost;
 
 
 
-// Recent external code snippet with css
+
+
+
+
+
+
+//CORRECTED CODE TO BE USED IN THE PROJECT
+
+
+// import img from '../../assets/team4.jpeg'
+
+// function OnePost() {
+//   return (
+//     <div className='py-10 px-6'>
+//       <div className="container mx-auto flex flex-col gap-6">
+        
+//         {/* Image Section */}
+//         <img
+//           src={img}
+//           alt="Post"
+//           className="w-full h-auto object-cover rounded-md shadow-lg"
+//         />
+
+//         {/* Textual Content Section - horizontal on md+ */}
+//         <div className="flex flex-col md:flex-row gap-6 items-start">
+
+//           {/* Title + Icons */}
+//           <div className="relative md:w-1/2">
+//             <h1 className="text-2xl font-bold mb-4">This is the Single Post Title</h1>
+//             <div className="absolute top-0 right-0 flex gap-3 text-gray-600">
+//               <i className="fa-solid fa-pen-to-square cursor-pointer hover:text-blue-600"></i>
+//               <i className="fa-solid fa-trash-can cursor-pointer hover:text-red-600"></i>
+//             </div>
+//           </div>
+
+//           {/* Post Info + Description */}
+//           <div className="md:w-1/2">
+//             <div className="flex flex-wrap gap-4 text-sm text-gray-500 mb-4">
+//               <span>Author: <b>Safari</b></span>
+//               <span>1 hour ago</span>
+//             </div>
+//             <p className="text-gray-700 leading-relaxed">
+//               The following is the singlePost Description. <br />
+//               The following is the singlePost Description. <br />
+//               The following is the singlePost Description.
+//             </p>
+//           </div>
+
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default OnePost;
+
+
+
+
+
+
+
+// RECENT CODE SNIPED WITH  CSS
 
 // import './singlePost.css'
 // import img from '../../assets/team4.jpeg'
