ad7c34c1e659df9a09f4dddf890debaa253f49ce
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-18 18:58:50 +0200

    Responsiveness in the blog post modified

--- my-react-app/src/components/post/Post.jsx
+++ my-react-app/src/components/post/Post.jsx
@@ -7,23 +7,23 @@ import React from 'react'
 
 function Post() {
   return (
-    <div className='container w-full mx-auto pt-10 py-4 md:px-20 lg:px-32 overflow-hidden bg-[#D9D9D9] rounded shadow-lg'>
+    <div className='w-full mx-auto pt-10 py-4 md:px-20 lg:px-32 overflow-hidden bg-[#D9D9D9] rounded shadow-lg mb-4'>
        {/* Title */}
-        <div className='pt-4 px-4 font-bold text-2xl'>
-          <h2 className='md:'>Most Viewed Post</h2>
+        <div className='font-bold text-2xl'>
+          <h2 className='pb-4'>Most <span className='underline underline-offset-4 decoration-1 font-light'>Viewed Post</span></h2>
         </div>
       {/* Image */}
       <img
-          className='w-full h-auto object-cover rounded-t'
+          className='w-full h-auto object-cover rounded-md shadow-lg'
           src={img}
           alt="Most viewed post"
         />
       
         
         {/* Buttons Section */}
-        <div className='flex justify-end pt-2 px-4 pb-4'>
-          <button className='border bg-white px-4 py-2 rounded'>23,473</button>
-          <button className='border bg-white px-4 py-2 rounded'>10,775</button>
+        <div className='flex justify-between pt-3 px-9 '>
+          <button className='border bg-white px-10 py-2 rounded'>23,473 $</button>
+          <button className='border bg-white px-10 py-2 rounded'>10,775 $</button>
         </div>
         
     </div>
