6609891877ca0e551ca87c91906f584d55f67fd8
olivier Rukundo <rr.olivier01@gmail.com>
2025-12-29 23:41:14 +0100

    Removed deprecated functions

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -5,7 +5,7 @@
 //   return (
 //     <div className='pt-10 pb-10 px-4 mb-6 md:mb-0 md:px-20 lg:px-32 w-full bg-gray-800 overflow-hidden'>    
 //             <div className='container flex flex-col justify-between md:flex-row mx-auto items-start '>
-//                 {/* 1st class */}
+//                 {/* 1st */}
 //                 <div className='w-full  md:w-1/3 mb-8 md:mb-0'>
 //                     <h3 className='text-white text-lg font-bold mb-8'>Info</h3>
 //                     <p className='text-gray-400'>we are ready to deliver quality services</p>                
