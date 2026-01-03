7a9bfa2fb9fcbce9712de95434358982e4835f24
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-03 23:16:35 +0100

    Fixed issue where comments were duplicated

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -21,7 +21,7 @@
 //                 </div>
 //                 {/* 3rd  */}
 //                 <div>
-//                   <h3 className='text-white'>Subscription.</h3>
+//                   <h3 className='text-white'>Subscription. data</h3>
 //                 </div>
 //             </div>
 //     </div>
