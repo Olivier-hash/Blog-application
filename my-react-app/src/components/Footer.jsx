86d9e30edd8d4bc9345a2901952355626d24ae4f
olivier Rukundo <rr.olivier01@gmail.com>
2026-01-04 20:01:07 +0100

    Added integration tests for authentication

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -21,7 +21,7 @@
 //                 </div>
 //                 {/* 3rd  */}
 //                 <div>
-//                   <h3 className='text-white'>Subscription. test</h3>
+//                   <h3 className='text-white'>Subscription. tes</h3>
 //                 </div>
 //             </div>
 //     </div>
