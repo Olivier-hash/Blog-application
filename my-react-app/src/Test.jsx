29b98c1673e89e8117240a6e3e46976376c70e0c
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-15 23:22:15 +0200

    added responsive container and spacing utilities

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 339-342)
+    // Animation properties (extra's 343-346)
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
