89958a8579ba1b7aec79d6db8a0e237fe39625fa
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-15 23:18:00 +0200

    created About section component for real estate landing page

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties
+    // Animation properties  (extra)
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
