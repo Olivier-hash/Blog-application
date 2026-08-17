b1e5ffea7a8fa2583fc250e5c561d2ce013834db
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-17 22:11:44 +0200

    integrated Motion for About section animations

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 343-346)  ed 
+    // Animation properties (extra's 343-346)  e
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
