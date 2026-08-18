e790988c80face7f38d1865aec12e258c7574adb
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-18 22:03:02 +0200

    triggered About animation only once per viewport

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 347-350)  extra
+    // Animation properties (extra's 347-350)  extr
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
