b1f201810d87dca948789e5baee84baee02d02fb
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-20 21:42:00 +0200

    refined image sizing across responsive breakpoints

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 347-350)  ext
+    // Animation properties (extra's 351-354)  ext
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
