0623e3c8997ec3e110a12670a7fa5befcc27a11a
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-21 22:43:37 +0200

    improved Learn More button presentation

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 355-358)  exC
+    // Animation properties (extra's 355-358)  ex
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
