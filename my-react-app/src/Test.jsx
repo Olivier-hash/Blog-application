cf20508063856767391007485809ab9357a5be2c
olivier Rukundo <rr.olivier01@gmail.com>
2026-08-21 22:40:55 +0200

    corrected company experience text and content wording

--- my-react-app/src/Test.jsx
+++ my-react-app/src/Test.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <motion.div
     
-    // Animation properties (extra's 355-358)  exCEL
+    // Animation properties (extra's 355-358)  exCE
     initial={{opacity:0, x:100}}
     transition={{duration: 1.5}}
     whileInView={{opacity:1, x:0}}
